dcf294cad1de266895ed95f88786c9039c82f06c
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-02-11 16:50:25 +0900

    first commit

---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div className="Login">
+    </div>
+  );
+}
+
+export default SignUp;
---
+++ src/pages/Todo/Todo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Todo() {
+  return (
+    <div className="Login">
+    </div>
+  );
+}
+
+export default Todo;

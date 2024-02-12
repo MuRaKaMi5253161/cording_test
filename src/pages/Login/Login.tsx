23349b485acfb068384cc05ce8e17d6832467f27
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-02-12 22:05:25 +0900

    ログインページレイアウト実装

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import './Login.css';
 
 function Login() {
+
+  const login = () => {
+    console.log("ログイン成功");
+    return;
+  }
+
   return (
     <div className="Login">
+      <div className='title'>
+            <h1>Login</h1>
+        </div>
+        <div className='main'>
+            <form onSubmit={login}>
+                <div className='mail'>
+                  <input type='mail' className='input' placeholder='mail' />
+                </div>
+
+                <div className='password'>
+                  <input type='pass' className='input' placeholder='pass' />
+                </div>
+
+                <input type='submit' className='submit' value="Login" />
+            </form>
+        </div>
     </div>
   );
 }

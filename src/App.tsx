130c3b853a705598172d38909d98fde58cc140d3
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-02-18 19:14:40 +0900

    ルーティング実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Login from './pages/Login/Login';
+import Todo from './pages/Todo/Todo';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Todo/>} />
+            <Route path='/Login' element={<Login/>} />
+            <Route path='/SignUp' element={<SignUp/>} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 

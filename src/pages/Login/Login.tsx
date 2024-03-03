b78b9f2daac3446ac90a7edc69847f5dd82a3e62
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-03-03 20:58:55 +0900

    ログイン機能実装

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import { useState } from 'react';
 import './Login.css';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+  const navigation = useNavigate();
 
-  const login = () => {
-    console.log("ログイン成功");
+  const SignIn = (event:any) => {
+    event.preventDefault();
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, mail, pass)
+      .then((userCredential) => {
+        alert("ログイン成功");
+        navigation("/");
+      })
+      .catch((error) => {
+        alert("ログイン失敗");
+        navigation("/Login");
+      });
     return;
   }
 
   return (
     <div className="Login">
       <div className='title'>
-            <h1>Login</h1>
+            <h1 className='titleText'>ログイン</h1>
         </div>
         <div className='main'>
-            <form onSubmit={login}>
+            <form onSubmit={SignIn}>
                 <div className='mail'>
-                  <input type='mail' className='input' placeholder='メールアドレスを入力してください' />
+                  <input type='mail' className='input' placeholder='メールアドレスを入力してください' onChange={(e) => setMail(e.target.value)} />
                 </div>
 
                 <div className='password'>
-                  <input type='pass' className='input' placeholder='パスワードを入力してください' />
+                  <input type='pass' className='input' placeholder='パスワードを入力してください' onChange={(e) => setPass(e.target.value)} />
                 </div>
 
                 <input type='submit' className='submit' value="Login" />

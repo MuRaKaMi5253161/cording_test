f35540e8c0f8a1e328b1bd550cb05831e7a567b1
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-03-06 23:29:32 +0900

    ヘッダーの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import './App.css'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from './pages/Login/Login';
-import Todo from './pages/Todo/Todo';
-import SignUp from './pages/SignUp/SignUp';
-import TodoHeader from './components/header/TodoHeader';
-import User from './pages/User/User';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Todo from "./pages/Todo/Todo";
+import SignUp from "./pages/SignUp/SignUp";
+import TodoHeader from "./components/header/TodoHeader";
+import User from "./pages/User/User";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<TodoHeader/>} />
+          <Route path="/" element={<TodoHeader />} />
+          <Route path="/User" element={<TodoHeader />} />
+          <Route path="/Login" element={<Header />} />
+          <Route path="/SignUp" element={<Header />} />
         </Routes>
         <div className="App">
           <Routes>
-            <Route path='/' element={<Todo/>} />
-            <Route path='/Login' element={<Login/>} />
-            <Route path='/SignUp' element={<SignUp/>} />
-            <Route path='/User' element={<User/>} />
+            <Route path="/" element={<Todo />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/User" element={<User />} />
           </Routes>
         </div>
       </Router>

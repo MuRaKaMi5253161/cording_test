6731e6fad21cf49e6330bfb1ffcf6014c1b60a79
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-03-10 22:45:21 +0900

    ユーザーページを実装

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import Profile from "../../img/defaultProfile.jpg";
 import "./User.css";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { auth, db, storage } from "../../firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, ref } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
 
 function User() {
@@ -13,38 +13,8 @@ function User() {
   const [birthday, setBirthday] = useState("");
   const [gender, setGender] = useState("");
   const [profileImage, setProfileImage] = useState(Profile);
-  const [profileImageFile, setProfileImageFile] = useState<any>();
 
   const navigation = useNavigate();
-
-  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-
-    const fileObject = e.target.files[0];
-    setProfileImage(window.URL.createObjectURL(fileObject));
-    setProfileImageFile(fileObject);
-  };
-
-  // ユーザー情報をfireStoreに保存
-  const sendUserInfo = (userId: string) => {
-    addDoc(collection(db, "users"), {
-      id: userId,
-      name: name,
-      mail: mail,
-      birthday: birthday,
-      gender: gender,
-    });
-    console.log(gender);
-    return;
-  };
-
-  const sendUserProfile = (userId: string) => {
-    const storageRef = ref(storage, "profileImg/" + userId + "/profileName");
-    uploadBytes(storageRef, profileImageFile).then((snapshot) => {
-      console.log("Uploaded a blob or file!");
-    });
-    return;
-  };
 
   const getUserProfile = (userId: string) => {
     const storageRef = ref(storage, "profileImg/" + userId + "/profileName");
@@ -64,7 +34,7 @@ function User() {
       console.log(doc.id, " => ", doc.data().name);
       setName(doc.data().name);
       setMail(doc.data().mail);
-      setBirthday(doc.data().birthDay);
+      setBirthday(doc.data().birthday);
       setGender(doc.data().gender);
     });
     return;
@@ -99,9 +69,7 @@ function User() {
       .then(() => {
         alert("ログアウトしました");
       })
-      .catch((error) => {
-        // An error happened.
-      });
+      .catch(() => {});
   };
 
   return (
@@ -115,68 +83,28 @@ function User() {
             <div className="profileImgBox">
               <img src={profileImage} className="profileImg" alt="" />
             </div>
-            <label className="profileLabel">
-              プロフィールを変更
-              <input
-                type="file"
-                accept="image/*"
-                onChange={onFileInputChange}
-                className="profileInput"
-              />
-            </label>
           </div>
 
-          <div className="name">
+          <div className="userName">
             <p className="inputNameTitle">お名前</p>
-            <input
-              type="text"
-              className="input"
-              placeholder="お名前を入力してください"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+            <p className="userValue">{name}</p>
           </div>
 
-          <div className="mail">
+          <div className="userMail">
             <p className="inputMailTitle">メールアドレス</p>
-            <input
-              type="mail"
-              className="input"
-              placeholder="メールアドレスを入力してください"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-            />
+            <p className="userValue">{mail}</p>
           </div>
 
-          <div className="birthDay">
+          <div className="userBirthDay">
             <p className="inputTitle">生年月日</p>
-            <br />
-            <input
-              type="date"
-              className="inputDate"
-              name="birthday"
-              value={birthday}
-              onChange={(e) => setBirthday(e.target.value)}
-            />
+            <p className="userValue">{birthday}</p>
           </div>
 
-          <div className="gender">
+          <div className="userGender">
             <p className="inputGenderTitle">性別</p>
-            <br />
-            <select
-              name="inputGender"
-              className="inputGender"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-            >
-              <option value="men" selected>
-                men
-              </option>
-              <option value="women">women</option>
-            </select>
+            <p className="userValue">{gender}</p>
           </div>
 
-          <input type="submit" className="submit" value="update" />
           <input
             type="button"
             className="logoutBtn"
